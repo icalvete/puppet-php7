6dff5169e4d50c434c4228959984a00b08b96a23
Israel Calvete <icalvete@gmail.com>
2016-10-24 09:37:09 +0200

    Adding forcetypes support.

---
+++ manifests/php7-fpm/config_forcetype.pp
@@ -0,0 +1,15 @@
+define php7::php7-fpm::config_forcetype (
+  $forcetypes = undef
+) {
+
+  include php7::php7-fpm
+
+  validate_array($forcetypes)
+
+  $forcetypes_4_shell = shell_join($forcetypes)
+  
+  exec{ 'config_www_pool_forcetype':
+    command => "/bin/sed -i -e \"s/.*security.limit_extensions = .php.*/security.limit_extensions = .php ${forcetypes_4_shell}/\" ${php7::params::php7_fpm_www_pool}",
+    unless  => "/bin/grep '\"security.limit_extensions = .php ${forcetypes_4_shell}\"' ${php7::params::php7_fpm_www_pool}"
+  }
+}
