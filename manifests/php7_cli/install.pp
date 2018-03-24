70eb5a8af0562e0c185f5fb2f4b8927bf7cd995b
Israel Calvete <icalvete@gmail.com>
2018-03-24 07:41:40 +0100

    Install maxminddb.so driver.

--- manifests/php7_cli/install.pp
+++ manifests/php7_cli/install.pp
@@ -11,4 +11,14 @@ class php7::php7_cli::install {
     content => template("${module_name}/cli_log.php.erb"),
     mode    => '0664',
   }
+
+  file { 'maxminddb.so':
+    ensure  => present,
+    path    => "${php7::params::extension_dir}/maxminddb.so",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0664',
+    source  => "puppet:///modules/${module_name}/extensions/maxminddb7.${php7::params::version}.so",
+    require => Package[$php7::params::php7_cli_package]
+  }
 }
