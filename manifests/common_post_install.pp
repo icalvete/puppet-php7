c765b477ab55d9285aadb51ccba51020c52a8edd
Israel Calvete <icalvete@gmail.com>
2018-12-04 17:14:33 +0100

    Complaint with ubuntu 18.04.

--- manifests/common_post_install.pp
+++ manifests/common_post_install.pp
@@ -1,19 +1,28 @@
 class php7::common_post_install inherits php7::params {
 
+  case $facts['os']['distro']['codename'] {
+    'bionic': {
+      $update_alternatives = '/usr/bin/update-alternatives'
+    }
+    default: {
+      $update_alternatives = '/usr/sbin/update-alternatives'
+    }
+  }
+
   exec { "update-alternatives_php7_${php7::version}":
-    command => "/usr/sbin/update-alternatives --set php /usr/bin/php7.${php7::version}",
+    command => "${update_alternatives} --set php /usr/bin/php7.${php7::version}",
     user    => 'root',
     require => Package[$php7::php7_cli_package]
   }
 
   exec { "update-alternatives_phar7_${php7::version}":
-    command => "/usr/sbin/update-alternatives --set phar /usr/bin/phar7.${php7::version}",
+    command => "${update_alternatives} --set phar /usr/bin/phar7.${php7::version}",
     user    => 'root',
     require => Package[$php7::php7_cli_package]
   }
 
   exec { "update-alternatives_phar.phar7_${php7::version}":
-    command => "/usr/sbin/update-alternatives --set phar.phar /usr/bin/phar.phar7.${php7::version}",
+    command => "${update_alternatives} --set phar.phar /usr/bin/phar.phar7.${php7::version}",
     user    => 'root',
     require => Package[$php7::php7_cli_package]
   }
